781ad5c59cfa8dd78ac0d2c9511a32f57c179280
Slobodan Moraca <moracaslobodan@gmail.com>
2021-09-02 16:15:14 +0200

    Added funcionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Todo from './components/Todo'
+
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Doktor u 7.45'/>
+      <Todo text='Sastanak u 14.30'/>
+      <Todo text='Rucak u 12.00'/>
+
+    </div>
+  );
 }
 
 export default App;
